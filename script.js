6ad0a9319704348baf4d5ca4ebb5c96da0251b52
JuniorSarh <sabelohgumede@gmail.com>
2025-07-11 12:13:17 +0200

    Table for BIOS page

--- script.js
+++
@@ -1,8 +0,0 @@
-function showDescription(text) {
-  document.getElementById('modal-text').textContent = text;
-  document.getElementById('modal').style.display = 'block';
-}
-
-function closeModal() {
-  document.getElementById('modal').style.display = 'none';
-}
